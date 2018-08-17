e5af819a7078f020b0bf8b1c9df33dce8204e999
Austin Lucas <aclucas196@gmail.com>
2018-08-17 08:33:56 -0500

    Initial Commit

---
+++ DevConnector/client/src/actions/authActions.js
@@ -0,0 +1,57 @@
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+import axios from "axios";
+import setAuthToken from "../utils/setAuthToken";
+import jwt_decode from "jwt-decode";
+// REGISTER
+
+export const registerUser = (userData, history) => dispatch => {
+  axios
+    .post("/api/users/register", userData)
+    .then(res => history.push("/login"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// LOGIN
+export const loginUser = userData => dispatch => {
+  axios
+    .post("/api/users/login", userData)
+    .then(res => {
+      // save to local storage
+      const { token } = res.data;
+      localStorage.setItem("jwtToken", token);
+      // set token to header
+      setAuthToken(token);
+      // decode the token to get user data
+      const decoded = jwt_decode(token);
+      // set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Logout User
+export const logoutUser = () => dispatch => {
+  // Remove the token from ls
+  localStorage.removeItem("jwtToken");
+  // Remove Auth header
+  setAuthToken(false);
+  // Set Current user to {} and isAuth... to false
+  dispatch(setCurrentUser({}));
+};
+
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
+};
---
+++ DevConnector/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: "mongodb://devuser:Rolltide1@ds115022.mlab.com:15022/devconnect",
+  secretOrKey: "secret"
+};
